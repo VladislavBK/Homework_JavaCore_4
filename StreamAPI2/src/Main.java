361b61bdc769a3008ebf5996007b7ad10adee6dc
Vladislav <vlad.bel.kos@mail.ru>
2022-08-08 17:10:36 +0300

    Add Project

---
+++ StreamAPI2/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)]));
+        }
+
+        long lowAgeStream = persons.stream()
+                .filter(age -> age.getAge() < 18)
+                .count();
+        System.out.println("Несовершеннолетних -> " + lowAgeStream);
+
+        List<String> highAgeStream = persons.stream()
+                .filter(highAge -> highAge.getAge() >= 18 && highAge.getAge() <= 27)
+                .map(Person::getFamily)
+                .collect(Collectors.toList());
+        System.out.println(highAgeStream);
+
+        List<Person> education = persons.stream()
+                .filter(woman -> woman.getEducation().equals(Education.HIGHER))
+                .filter(woman -> woman.getSex().equals(Sex.WOMAN) == woman.getAge() >= 18 && woman.getAge() <= 60)
+                .filter(man -> man.getEducation().equals(Education.HIGHER))
+                .filter(man -> man.getSex().equals(Sex.MAN) == man.getAge() >= 18 && man.getAge() <= 65)
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
+
+
+    }
+
+}
